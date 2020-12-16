aad3795ecb581152af09e9bfab42c61d30be63de
Roman <rs.ponasenko@gmail.com>
2020-12-16 10:53:49 +0000

    style: extra spaces remove

--- module/phonebook_store.h
+++ module/phonebook_store.h
@@ -4,7 +4,7 @@
 #include "kernel_phonebook.h"
 
 int pb_get(const char *surname, unsigned int len, struct pb_user_data *data);
-int pb_add(const struct pb_user_data *data);	
+int pb_add(const struct pb_user_data *data);
 int pb_del(const char *surname, unsigned int len);
 
 #endif // PHONEBOOK_STORE_H
